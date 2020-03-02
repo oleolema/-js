35c90b910d886c590ee562d3d75e4fdf09ad39dc
oleolema <1075056431@qq.com>
2020-03-02 22:57:49 +0800

    Update SwipeImg.js
    添加：可以对元素轮播，切换按钮

--- js/SwipeImg.js
+++ js/SwipeImg.js
@@ -1,15 +1,22 @@
 {
+
     /**
      * @param imgbox        图片容器的class
      * @param resources     图片资源
      * @param isColorImg    （可选，默认 false）是否为纯颜色
      */
-    window.SwipeImg = function (imgbox, resources, isColorImg) {
+    window.SwipeImg = function (imgbox, resources, {
+        type,
+        button,
+    }) {
         this.imgbox = $(imgbox); //图片外层
         this.resources = resources; //资源   (默认资源图片资源)
         this.swipeListener = function () {};
         this.imgCache = [];
         this.imgCacheCount = 5;
+        this.buttonClass = button === true ? ['pre-img-btn', 'next-img-btn', 'img-btn'] : button;
+
+        this.swipeTimer = 0;
 
         this.index = 0;
         this.imgClass = ["swipe-img1" + randomStr(), "swipe-img2" + randomStr(), "swipe-img3" + randomStr()];
@@ -20,10 +27,15 @@
         }, {
             transform: "translate(100%, 0px)"
         }, ];
-        if (isColorImg) { //采用哪种方式加载资源
+        if (type === SwipeImg.TYPE.color) { //采用哪种方式加载资源
             this.setNextResource = function (img) {
                 img.css("background-color", this.resources[this.index++ % this.resources.length]);
             };
+        } else if (type === SwipeImg.TYPE.element) {
+            this.setNextResource = function (img) {
+                img[0].innerHTML = (this.resources[this.index++ % this.resources.length]);
+            };
+            this.imgCacheCount = 0;
         } else {
             this.setNextResource = function (img) {
                 img.css("background-image", `url(${ this.resources[this.index++ % this.resources.length]})`);
@@ -31,6 +43,11 @@
         }
         //初始化
         this.init();
+    }
+
+    SwipeImg.TYPE = {
+        color: Symbol(),
+        element: Symbol(),
     }
 
     function randomStr() {
@@ -99,20 +116,27 @@
         .${this.imgClass[2]}{
             ${tempTransform[2]}
         }</style>`);
+
         $('head').append(this.cssStyle);
     }
 
 
     //调用show方法才会开始轮播
     SwipeImg.prototype.show = function (t, delay) {
-        t = t || 2000;
+        this.t = t || 2000;;
         delay = delay || 0;
-        //加载图片动画效果
-        this.loadTransform();
         setTimeout(() => {
-            setInterval(() => {
-                this.swipeListener();
-                //图片资源开启缓存
+            this.autoNext();
+        }, delay);
+        return this;
+    }
+
+    SwipeImg.prototype.autoNext = function () {
+
+        this.swipeTimer = setInterval(() => {
+            this.swipeListener();
+            //图片资源开启缓存
+            if (this.imgCacheCount !== 0) {
                 if (this.index == 1 && !this.isColorImg) {
                     for (var i = 0; i < this.resources.length && i < this.imgCacheCount; i++) {
                         this.imgCache[i] = $(`<img src="${this.resources[i]}"  style="width:0px;height:0px;" >`);
@@ -121,14 +145,25 @@
                 } else if (this.index + this.imgCacheCount < this.resources.length) {
                     this.imgCache[this.index % this.imgCacheCount].attr("src", this.resources[this.index + this.imgCacheCount]);
                 }
-
-                //显示这个class
-                this.setNextClass(this.imgClass[0], this.imgClass[1], this.imgClass[2]);
-            }, t);
-        }, delay);
-        return this;
+            }
+            //显示这个class
+            this.setNextClass(this.imgClass[0], this.imgClass[1], this.imgClass[2]);
+        }, this.t);
     }
 
+    SwipeImg.prototype.pre = function () {
+        this.swipeListener();
+        clearInterval(this.swipeTimer);
+        this.setNextClass(this.imgClass[2], this.imgClass[1], this.imgClass[0]);
+        this.autoNext();
+    }
+
+    SwipeImg.prototype.next = function () {
+        this.swipeListener();
+        clearInterval(this.swipeTimer);
+        this.setNextClass(this.imgClass[0], this.imgClass[1], this.imgClass[2]);
+        this.autoNext();
+    }
 
 
     SwipeImg.prototype.setNextClass = function (preClass, hidenClass, showClass) {
@@ -151,12 +186,18 @@
 
     //初始化
     SwipeImg.prototype.init = function () {
+
+        //加载图片动画效果
+        this.loadTransform();
+
+        let btnCl = this.buttonClass;
         //生成3张图片
         var img1 = $(`<div class="${this.imgClass[0]}"></div>`);
         var img2 = $(`<div class="${this.imgClass[1]}"></div>`);
         var img3 = $(`<div class="${this.imgClass[2]}"></div>`);
 
         this.imgbox.append(img1, img2, img3);
+
         //中间一张展示图片
         this.setNextResource(img2);
 
@@ -168,6 +209,8 @@
             margin: "0px auto",
             "perspective-origin": "center",
             "perspective": "1500px",
+            display: 'flex',
+            'align-items': 'center',
         });
         this.setImgCss({
             height: "100%",
@@ -177,71 +220,38 @@
             "background-repeat": "no-repeat",
             "background-position": "center",
             "background-size": "cover",
+
         });
 
-
-
-
+        if (btnCl) {
+            let pre = $(`<div class='${btnCl[0]} ${btnCl[2]}'></div>`);
+            let next = $(`<div class='${btnCl[1]} ${btnCl[2]}'><</div>`);
+            pre.text('⬅️');
+            next.text('➡️');
+            pre.click(() => {
+                this.pre();
+            });
+            next.click(() => {
+                this.next();
+            })
+            // pre.css()
+            this.imgbox.append(pre, next);
+            $('head').append(`<style>
+                .${this.buttonClass[2]}{
+                    position: absolute;
+                    width: 20px;
+                    height: 20px;
+                    cursor: pointer;
+                    opacity: 0.8;
+                }
+                .${this.buttonClass[0]}{
+                    left:0;
+                }
+                .${this.buttonClass[1]}{
+                    right:0;
+                }
+                </style>`)
+        }
     }
-    //使用方法用法
-
-    /*
-    var imgSrc = ["img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg", "img/5.jpg", "img/6.jpg", "img/7.jpg"];
-    var transform = [{
-        transform: "translate(-100%, 0%)"
-    }, {
-        transform: "translate(0px, 0px)  scale(1.2)"
-    }, {
-        transform: "translate(100%,0%)"
-    }];
-
-    var transform1 = [{
-        transform: "translate3d(-100%, 0%,-500px) rotateY(-90deg)"
-    }, {
-        transform: "translate3d(0%, 0%,0px) rotateY(0deg)"
-    }, {
-        transform: "translate3d(100%, 0%,-500px) rotateY(90deg)"
-    }];
-
-
-    var imgColor = ["red", "green", "blue"];
-
-    new SwipeImg(".imgbox", imgSrc)
-        .setTransform(transform1)
-        .setImgBoxCss({
-            width: "50%",
-            height: "300px",
-            "margin-top": "200px",
-            "perspective-origin": "center",
-            "perspective": "1500px",
-            position: "relative",
-            bottom:"100px",
-        }).setImgCss({
-
-        })
-        .setImgCss({
-
-        })
-        .show(1500);
-
-    var swipeImg = new SwipeImg(".imgbox2", imgSrc)
-        .setTransform(transform)
-        .setImgBoxCss({
-            height: "500px",
-            position: "absolute",
-            "z-index": "-1",
-            bottom: "0px",
-            filter: "blur(10px)",
-            "perspective-origin": "center",
-            "perspective": "1500px"
-        }).setImgCss({
-
-        })
-        .setSwipeListener(function () {
-            // randomSwipeImg1.bind(this)();
-        })
-        .show(1500);
-
-        */
 
 }
